ff0dea3e7cbbfb497bddf9c82967a0227bb15db3
“takane2497“ <takane.konagaya@gmail.com>
2023-02-12 14:27:24 +0900

    takonaga add ft_strndup.c in libft add x_strndup

--- takonaga/libft/libft.h
+++ takonaga/libft/libft.h
@@ -41,6 +41,7 @@ char			*ft_strnstr(const char *str, const char *seek, size_t len);
 int				ft_atoi(const char *nptr);
 void			*ft_calloc(size_t count, size_t size);
 char			*ft_strdup(const char *s1);
+char 			*ft_strndup(const char *s1, size_t n);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
