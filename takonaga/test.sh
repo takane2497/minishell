8392818f2eff53f60d52d631f14a9a58314e19f8
“takane2497“ <takane.konagaya@gmail.com>
2023-02-12 18:45:34 +0900

    takonaga add test.sh

--- takonaga/test.sh
+++ takonaga/test.sh
@@ -2,15 +2,23 @@
 
 cat <<EOF | gcc -xc -o a.out -
 #include <stdio.h>
-int main() {printf("hello from a.out"); }
+int main() { printf("hello from a.out\n"); }
+EOF
+
+cat <<EOF | gcc -xc -o print_args -
+#include <stdio.h>
+int main(int argc, char *argv[]) {
+	for (int i = 0; argv[i]; i++)
+		printf("argv[%d] = %s\n", i, argv[i]);
+}
 EOF
 
 cleanup() {
-	rm -f cmp out a.out
+	rm -f cmp out a.out print_args
 }
 
 assert() {
-	printf '%-30s:' "\"$1\""
+	printf '%-50s:' "\"$1\""
 	# exit status
 	echo -n -e "$1" | bash >cmp 2>&-
 	expected=$?
@@ -35,18 +43,29 @@ assert '/bin/pwd'
 assert '/bin/echo'
 assert '/bin/ls'
 
-# Generate Executable
-
 # Search command path without args
 assert 'pwd'
 assert 'echo'
 assert 'ls'
 assert './a.out'
-assert 'cat'
 
-# no such command
+## no such command
 assert 'a.out'
 assert 'nosuchfile'
+
+# Tokenize
+## unquoted word
+assert 'ls /'
+assert 'echo hello    world     '
+assert 'nosuchfile\n\n'
+
+## single quote
+assert "./print_args 'hello   world' '42Tokyo'"
+assert "echo 'hello   world' '42Tokyo'"
+assert "echo '\"hello   world\"' '42Tokyo'"
+
+## combination
+assert "echo hello'      world'"
 
 cleanup
 echo 'all OK'
